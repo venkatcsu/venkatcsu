5c55cb8a1f59232d8c6e9c2bc18ec8c908fdb3e2
venkatcsu <venkatesan.alagarsamy@csuglobal.edu>
2024-10-11 22:07:54 -0400

    Add files via upload

---
+++ Module1/Addition.py
@@ -0,0 +1,6 @@
+number1 = int(input("Enter the first number: ")) 
+number2 = int(input("Enter second number: ")) 
+
+total = number1 + number2
+print('Addition of',number1,'and',number2,'Total:', total) 
+
---
+++ Module1/Subtraction.py
@@ -0,0 +1,6 @@
+number1 = int(input("Enter the first number: ")) 
+number2 = int(input("Enter second number: ")) 
+
+updtnumber = number1 -  number2
+print('Current value:', updtnumber) 
+
